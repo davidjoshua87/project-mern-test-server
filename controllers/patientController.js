7cf4b7fb01f57ab964b7aa8dee701e2d7f624458
davidjoshua87 <davidjosh_87@yahoo.com>
2022-09-22 00:15:37 +0700

    fix: #1 fix controller update patient.

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -3,23 +3,25 @@ const errorHandler = require('./errorController');
 
 
 // Display All Patient Data
-const patient_list = async (req, res) => {
-	Patient.find((err, response) => {
-		if (!response) {
-			res.status(404).json({
-				message: "No Result Found",
-			})
-		} else {
-			res.status(200).json({
-				data: response,
-				message: "Successfully Get All Data",
-			})
+const patient_list = (req, res) => {
+	Patient.find((err, data) => {
+		if (!err) {
+			if (!data) {
+				res.status(404).json({
+					message: "No Result Found",
+				})
+			} else {
+				res.status(200).json({
+					data: data,
+					message: "Successfully Get All Data",
+				})
+			}
 		}
 	});
 };
 
 // Create New Patient
-const patient_create_post = async(req, res) => {
+const patient_create_post = async (req, res) => {
 	let patient = new Patient(req.body);
 	patient
 		.save()
@@ -41,68 +43,85 @@ const patient_create_post = async(req, res) => {
 };
 
 // Show a particular Patient Detail by Id
-const patient_details = async (req, res) => {
-	Patient.findById(req.params.id, (err, response) => {
-		if (!response) {
-			res.status(404).json({
-				message: "No Result Found",
-			})
-		} else {
-			res.status(200).json({
-				data: response,
-				message: "Successfully Get Data By Id",
-			})
+const patient_details = (req, res) => {
+	Patient.findById(req.params.id, (err, data) => {
+		if (!err) {
+			if (!data) {
+				res.status(404).json({
+					message: "No Result Found",
+				})
+			} else {
+				res.status(200).json({
+					data: data,
+					message: "Successfully Get Data By Id",
+				})
+			}
 		}
 	});
 };
 
 // Update Patient Detail by Id
 const patient_update = (req, res) => {
-	Patient.findById(req.params.id, (err, response) => {
-		if (req.body.__v === response.__v) {
-			Patient.save(req.body)
-			.then((data) => {
-				res.status(200).json({
-					data: data,
-					message: "Successfully Update Patient",
-				})
-			})
-			.catch((err) => {
-				if (err.code === 11000) {
-					let data = errorHandler.duplicateKeyError(err, res);
-					res.status(data.code).json({
-						message: data.messages,
-						fields: data.fields
+	Patient.findOneAndUpdate({
+			_id: req.params.id
+		}, req.body, {
+			new: true,
+			upsert: true,
+		})
+		.then((update) => {
+			if (update.__v === req.body.__v) {
+				let patient = new Patient(update);
+				patient
+					.save()
+					.then((data) => {
+						res.status(200).json({
+							data: data,
+							message: "Successfully Update Patient",
+						})
 					})
-				}
-			});
-		} else {
-			res.status(412).json({
-				message: "Precondition Failed",
+					.catch((err) => {
+						if (err.code === 11000) {
+							let data = errorHandler.duplicateKeyError(err, res);
+							res.status(data.code).json({
+								message: data.messages,
+								fields: data.fields
+							})
+						}
+					});
+			} else {
+				res.status(412).json({
+					message: "Precondition Failed",
+				})
+			}
+		})
+		.catch((err) => {
+			res.status(422).json({
+				message: err,
 			})
-		}
-	});
+		});
 };
 
 // Delete Patient Detail by Id
 const patient_delete = (req, res) => {
 	Patient.findById(req.params.id, (err, data) => {
-		if (!data) {
-			res.status(404).json({
-				message: "No Result Found",
-			})
-		} else {
-			Patient.findByIdAndRemove(req.params.id)
-				.then(() => {
-					res.status(200).json({
-						message: "Successfully Delete Patient",
-					})
+		if (!err) {
+			if (!data) {
+				res.status(404).json({
+					message: "No Result Found",
 				})
-				.catch((err) => {
-					res.status(400).json({
-						message: "Patient delete failed",
+			} else {
+				Patient.findByIdAndRemove(req.params.id)
+					.then(() => {
+						res.status(200).json({
+							message: "Successfully Delete Patient",
+						})
 					})
-				});
+					.catch((err) => {
+						res.status(400).json({
+							message: "Patient delete failed",
+						})
+					});
+			}
 		}
 	});
 };
